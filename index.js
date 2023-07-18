bd079e35d94afe7f2b0dea009ba1cce176bee725
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-07-18 11:50:34 +0900

    테스트

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var media = matchMedia('screen and (max-width:991px)');
 
 function Mobile(){return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);}
 
-var agent = navigator.userAgent.toLowerCase();
+var agent = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
 
 if(Mobile()){
     if( agent.indexOf('android') > -1 ) {
         
     }else if( agent.indexOf("iphone") > -1 || agent.indexOf("ipad") > -1 || agent.indexOf("ipod") > -1 ) {
-        if(media.matches){
+        
             document.querySelector('.gif-box-inner').style.background = `url(../asstes/img/test.gif) no-repeat center / cover`;
-        }
+        
     }else{
         
     }
