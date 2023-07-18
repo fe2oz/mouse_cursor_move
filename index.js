a986cf8db1adeff8497dfa0a61439a9649a75c25
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-07-18 11:17:52 +0900

    테스트

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-var cursor = document.querySelectorAll('.cursor');
-var section = document.querySelectorAll('.section');
 
-for(var i=0; i<section.length; i++){
-    section[i].addEventListener('mouseover', function(){
-        var item = this;
-        for(var i=0; i<section.length; i++){
-            if(item != section[i]){
-                cursor[i].classList.remove('test');
-            }else if(item.classList.contains('test') === true){
-                cursor[i].classList.remove('test');
-            }else{
-                cursor[i].classList.add('test');
-            }
-        }
-    });
-    section[i].addEventListener('mouseout', function(){
-        for(var i=0; i<cursor.length; i++){
-            cursor[i].classList.remove('test');
-        }
-    });
+
+const ios = document.querySelector('.gif-box.ios');
+
+var agent = navigator.userAgent.toLowerCase();
+
+if( agent.indexOf('android') > -1 ) {
+    ios.style.display = 'none';
+}else if( agent.indexOf("iphone") > -1 || agent.indexOf("ipad") > -1 || agent.indexOf("ipod") > -1 ) {
+    ios.style.display = 'block';
+}else{
+    ios.style.display = 'none';
 }
-
-window.addEventListener('mousemove', function(e){
-    var this_cursor = document.querySelector('.test');
-    var posX = 'translate3d(' + e.clientX + 'px, ' + e.clientY + 'px, 0)'
-    
-    this_cursor.style.transform = posX;
-})
