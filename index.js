7044cb767e0c665c58b2c5c3dcc7a7ac8c94e917
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-07-18 12:19:39 +0900

    ㅇㅇ

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 //var media = matchMedia('screen and (max-width:991px)');
-//function Mobile(){return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);}
+function Mobile(){return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);}
 
 var agent = navigator.userAgent.toLowerCase(); //userAgent 값 얻기
 
-if( agent.indexOf('android') > -1 ) {
-    
-}else if( agent.indexOf("iphone") > -1 || agent.indexOf("ipad") > -1 || agent.indexOf("ipod") > -1 ) {
-    document.querySelector('.gif-box-inner').style.background = `url('../asstes/img/test.gif') no-repeat center / cover`;
-}else{
-    
+console.log(Mobile());
+if(Mobile() === true){
+
+        
+
+        document.querySelector('.gif-box-inner').style.background = `url('../asstes/img/test.gif') no-repeat center / cover`;
+
 }
+    
 
