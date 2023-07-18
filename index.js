93d5feda9c63e7c1ce33071e37c85900ecbb720a
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-07-18 11:20:40 +0900

    테스트

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 
 
 const ios = document.querySelector('.gif-box.ios');
+const iosEtc = document.querySelector('.gif-box.iosEtc');
 
 var agent = navigator.userAgent.toLowerCase();
 
 if( agent.indexOf('android') > -1 ) {
     ios.style.display = 'none';
+    iosEtc.style.display = 'block';
 }else if( agent.indexOf("iphone") > -1 || agent.indexOf("ipad") > -1 || agent.indexOf("ipod") > -1 ) {
     ios.style.display = 'block';
+    iosEtc.style.display = 'none';
 }else{
     ios.style.display = 'none';
+    iosEtc.style.display = 'block';
 }
